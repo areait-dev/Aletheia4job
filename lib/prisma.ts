88de2d4797bd2adfcc249143383c4689d049890b
seo-hash <seo@madroom.it>
2026-05-13 16:16:20 +0200

    Fix Prisma export compatibility

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -13,6 +13,6 @@ declare global {
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+export const prisma = globalThis.prisma ?? prismaClientSingleton()
 export default prisma
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
